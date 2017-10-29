216b884cb42906006d1abc0bada4f0bd98bbddad
Hugo Bragança da Silva <hugobraganca94@gmail.com>
2017-10-29 15:15:53 -0200

    FlexBox
    Divisão da área da tela utilizando flexbox

--- index.android.js
+++ index.android.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View } from 'react-native';
 
 //Formatações
 const Estilos =  {
-  estiloTexto: {
-    fontSize: 50
-  }
+    principal:{
+        flex: 1, //1:1
+        backgroundColor: 'cornflowerblue'
+    },
+
+    topo:{
+        flex: 2,//2:11
+        backgroundColor: 'brown'
+    },
+    conteudo:{
+        flex: 8,//8:11
+        backgroundColor: 'yellowgreen'
+    },
+    rodape:{
+        flex: 1,//1:11
+        backgroundColor: 'orangered'
+    }
+
 };
 
+
+//Criar  o componente
 const App = () => {
+
+  const { principal, topo, conteudo, rodape } = Estilos;
+
   return(
-      <Text style={ Estilos.estiloTexto }>Frases do dia</Text>
+      <View style={principal}>
+      <Text style={ topo }>Topo</Text>
+      <Text style={ conteudo }>Conteudo</Text>
+      <Text style={ rodape }>Rodape</Text>
+      </View>
     );
 };
 
